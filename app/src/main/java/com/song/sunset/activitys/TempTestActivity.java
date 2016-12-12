93dba43626fec00d710ebb3049f05fa115153dd1
songmw3 <songmw3@ifeng.com>
2016-12-12 18:37:33 +0800

    scrollview 反射设置 偏移距离

--- app/src/main/java/com/song/sunset/activitys/TempTestActivity.java
+++ app/src/main/java/com/song/sunset/activitys/TempTestActivity.java
@@ -1,26 +1,12 @@
 package com.song.sunset.activitys;
 
-import android.app.Activity;
-import android.app.Notification;
-import android.content.Intent;
-import android.content.res.Configuration;
-import android.database.Cursor;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.os.PersistableBundle;
-import android.provider.MediaStore;
-import android.util.Log;
 import android.view.View;
-import android.widget.ImageView;
 
 import com.song.sunset.R;
+import com.song.sunset.Reflect;
+import com.song.sunset.utils.ViewUtil;
 import com.song.sunset.views.CustomScrollView;
-
-import java.io.File;
-import java.util.Date;
 
 /**
  * Created by songmw3 on 2016/12/2.
@@ -35,9 +21,11 @@ public class TempTestActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_temp_test);
         scrollView = (CustomScrollView) findViewById(R.id.scrollView);
+        scrollView.setOverScrollMode(View.OVER_SCROLL_ALWAYS);
+        Reflect.on(scrollView).set("mOverflingDistance", ViewUtil.getScreenHeigth() / 8);
     }
 
-    public void onClick(View view) {
+    public void onTempTestFlowButtonClick(View view) {
         scrollView.addViews();
     }
 }
--- app/src/main/java/com/song/sunset/views/ImageSpanView.java
+++ app/src/main/java/com/song/sunset/views/ImageSpanView.java
@@ -30,60 +30,6 @@ public class ImageSpanView extends RelativeLayout {
     }
 
     @Override
-    protected void onAttachedToWindow() {
-        super.onAttachedToWindow();
-        Rect rect = new Rect();
-        getGlobalVisibleRect(rect);
-        Log.i("-----Attached ：", "top" + rect.top + " ; bottom" + rect.bottom);
-
-        int height = getHeight();
-        int[] arrayOfInt = new int[2];
-        this.getLocationOnScreen(arrayOfInt);
-        int j = arrayOfInt[1];
-        if ((j < ViewUtil.getScreenHeigth()) && (j + height > 0)) {
-            Log.i("-----Attached ：", "在" + "height = " + height + " , top = " + j);
-        } else {
-            Log.i("-----Attached ：", "不在" + "height = " + height + " , top = " + j);
-        }
-    }
-
-    @Override
-    protected void onFinishInflate() {
-        super.onFinishInflate();
-        Rect rect = new Rect();
-        getGlobalVisibleRect(rect);
-        Log.i("-----Finish ：", "top" + rect.top + " ; bottom" + rect.bottom);
-
-        int height = getHeight();
-        int[] arrayOfInt = new int[2];
-        this.getLocationOnScreen(arrayOfInt);
-        int j = arrayOfInt[1];
-        if ((j < ViewUtil.getScreenHeigth()) && (j + height > 0)) {
-            Log.i("-----Finish ：", "在" + "height = " + height + " , top = " + j);
-        } else {
-            Log.i("-----Finish ：", "不在" + "height = " + height + " , top = " + j);
-        }
-    }
-
-    @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        Rect rect = new Rect();
-        getGlobalVisibleRect(rect);
-        Log.i("-----onMeasure ：", "top" + rect.top + " ; bottom" + rect.bottom);
-
-        int height = getHeight();
-        int[] arrayOfInt = new int[2];
-        this.getLocationOnScreen(arrayOfInt);
-        int j = arrayOfInt[1];
-        if ((j < ViewUtil.getScreenHeigth()) && (j + height > 0)) {
-            Log.i("-----onMeasure ：", "在" + "height = " + height + " , top = " + j);
-        } else {
-            Log.i("-----onMeasure ：", "不在" + "height = " + height + " , top = " + j);
-        }
-    }
-
-    @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
         super.onLayout(changed, l, t, r, b);
         Rect rect = new Rect();
@@ -98,24 +44,6 @@ public class ImageSpanView extends RelativeLayout {
             Log.i("-----onLayout ：", "在" + "height = " + height + " , top = " + j);
         } else {
             Log.i("-----onLayout ：", "不在" + "height = " + height + " , top = " + j);
-        }
-    }
-
-    @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-        Rect rect = new Rect();
-        getGlobalVisibleRect(rect);
-        Log.i("-----onDraw ：", "top" + rect.top + " ; bottom" + rect.bottom);
-
-        int height = getHeight();
-        int[] arrayOfInt = new int[2];
-        this.getLocationOnScreen(arrayOfInt);
-        int j = arrayOfInt[1];
-        if ((j < ViewUtil.getScreenHeigth()) && (j + height > 0)) {
-            Log.i("-----onDraw ：", "在" + "height = " + height + " , top = " + j);
-        } else {
-            Log.i("-----onDraw ：", "不在" + "height = " + height + " , top = " + j);
         }
     }
 
