714ea31fb3298082f23548ded0c3f7e575464857
songmw3 <songmw3@ifeng.com>
2016-12-13 17:05:12 +0800

    。

--- app/src/main/java/com/song/sunset/activitys/TempTestActivity.java
+++ app/src/main/java/com/song/sunset/activitys/TempTestActivity.java
@@ -2,7 +2,9 @@ package com.song.sunset.activitys;
 
 import android.os.Bundle;
 import android.view.View;
-
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import com.song.sunset.R;
 import com.song.sunset.Reflect;
 import com.song.sunset.utils.ViewUtil;
@@ -15,14 +17,20 @@ import com.song.sunset.views.CustomScrollView;
 public class TempTestActivity extends BaseActivity {
 
     private CustomScrollView scrollView;
+    private ProgressBar progressBar;
+    private TextView percent;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_temp_test);
         scrollView = (CustomScrollView) findViewById(R.id.scrollView);
+        progressBar = (ProgressBar) findViewById(R.id.progress);
+        button = (Button) findViewById(R.id.button);
+        percent = (TextView) findViewById(R.id.percent);
         scrollView.setOverScrollMode(View.OVER_SCROLL_ALWAYS);
-        Reflect.on(scrollView).set("mOverflingDistance", ViewUtil.getScreenHeigth() / 8);
+        Reflect.on(scrollView).set("mOverflingDistance", ViewUtil.getScreenHeigth() / 16);
     }
 
     public void onTempTestFlowButtonClick(View view) {
