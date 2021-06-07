62258d9bca73eb753a8b2e1b197fd42a94c5906c
mingwen <mingwen@mgtv.com>
2021-06-07 18:41:50 +0800

    解决 release 打包问题

--- app/proguard-rules.pro
+++ app/proguard-rules.pro
@@ -15,6 +15,8 @@
 #-keepclassmembers class fqcn.of.javascript.interface.for.webview {
 #   public *;
 #}
+# 抑制警告
+-ignorewarnings
 
 -dontwarn com.tencent.bugly.**
 -keep public class com.tencent.bugly.**{*;}
--- design/src/test/java/com/song/sunset/design/CompositeTest.kt
+++ design/src/test/java/com/song/sunset/design/CompositeTest.kt
@@ -7,7 +7,9 @@ import com.song.sunset.design.structural.composite.IceCream
 import org.junit.Test
 
 /**
- * Desc:
+ * Desc:    组合模式（Composite Pattern），又叫部分整体模式，是用于把一组相似的对象当作一个单一的对象。
+ *          组合模式依据树形结构来组合对象，用来表示部分以及整体层次。这种类型的设计模式属于结构型模式，
+ *          它创建了对象组的树形结构。
  * Author:  songmingwen
  * Email:   mingwen@mgtv.com
  * Time:    2021/5/11 11:12
